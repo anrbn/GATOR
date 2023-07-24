62bea2228aed1150d32d0825d5243298ebf03cb1
Anirban Das <danirban205@gmail.com>
2023-07-24 23:31:38 +0530

    v0.5.2

--- gator/arg_parsers/iam_parser.py
+++ gator/arg_parsers/iam_parser.py
@@ -1,14 +1,25 @@
 from argparse import ArgumentParser
 
 from gator.services.iam.service_accounts import list_service_accounts
+from gator.services.iam.iam import set_iam_policy
 
 def iam_parser(parent_parser):
     iam_parser = ArgumentParser(add_help=False, parents=[parent_parser])
+    subparsers = iam_parser.add_subparsers()
 
-    service_accounts_parser = iam_parser.add_subparsers().add_parser('service-accounts', parents=[parent_parser])
-    list_service_accounts_parser = service_accounts_parser.add_subparsers().add_parser('list', parents=[parent_parser])
+    service_accounts_parser = subparsers.add_parser('service-accounts', parents=[parent_parser])
+    service_accounts_subparsers = service_accounts_parser.add_subparsers()
 
+    list_service_accounts_parser = service_accounts_subparsers.add_parser('list')
     list_service_accounts_parser.add_argument('--project-id', required=True, help='The project ID.')
     list_service_accounts_parser.set_defaults(func=list_service_accounts)
+
+    set_iam_policy_parser = subparsers.add_parser('set-iam-policy', parents=[parent_parser])
+    set_iam_policy_parser.add_argument('--project-id', required=True, help='The project ID.')
+    set_iam_policy_parser.add_argument('--function-name', required=True, help='The name of the function.')
+    set_iam_policy_parser.add_argument('--region', required=True, help='The region of the function.')
+    set_iam_policy_parser.add_argument('--member', required=True, help='The member to add to the policy.')
+    set_iam_policy_parser.add_argument('--role', required=True, help='The role to give to the member.')
+    set_iam_policy_parser.set_defaults(func=set_iam_policy)
 
     return iam_parser
---
+++ gator/services/iam/iam.py
@@ -0,0 +1,47 @@
+from googleapiclient import discovery
+from googleapiclient.errors import HttpError
+from gator.auth.credentials import load_credentials
+
+def create_policy(args):
+    # Determine the member type based on the input
+    if args.member.endswith(".gserviceaccount.com"):
+        member = f"serviceAccount:{args.member}"
+    elif "@" in args.member:
+        member = f"user:{args.member}"
+    else:
+        member = args.member
+
+    policy = {
+        "bindings": [
+            {
+                "role": args.role,
+                "members": [member]
+            }
+        ]
+    }
+
+    return policy
+
+
+def set_iam_policy(args):
+    creds = load_credentials(args)
+
+    # Check if function_name is provided
+    if args.function_name:
+        service = discovery.build('cloudfunctions', 'v1', credentials=creds)
+        name = f"projects/{args.project_id}/locations/{args.region}/functions/{args.function_name}"
+
+        policy = create_policy(args)
+
+        try:
+            request = service.projects().locations().functions().setIamPolicy(
+                resource=name,
+                body={
+                    'policy': policy,
+                }
+            )
+            request.execute()
+            print(f"IAM policy set for function {args.function_name}.")
+        except HttpError as error:
+            print(f"An error occurred: {error}")
+
